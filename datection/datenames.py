19f7530939cc0450f05ce023a509de7ab168e741
Balthazar Rouberol <balthazar.rouberol@mapado.com>
2013-02-21 14:16:32 +0100

    All name → number conversion rules moved to datenames.py
    Signed-off-by: Balthazar Rouberol <balthazar.rouberol@mapado.com>

---
+++ datection/datenames.py
@@ -0,0 +1,73 @@
+# -*- coding: utf8 -*-
+
+
+WEEKDAY_VALUE = {
+    'fr':
+    {
+        'lundi': 1,
+        'mardi': 2,
+        'mercredi': 3,
+        'jeudi': 4,
+        'vendredi': 5,
+        'samedi': 6,
+        'dimanche': 7,
+    }
+}
+
+SHORT_WEEKDAY_VALUE = {
+# Untested for now!
+    'fr':
+    {
+        'lun': 1,
+        'mar': 2,
+        'mer': 3,
+        'jeu': 4,
+        'ven': 5,
+        'sam': 6,
+        'dim': 7,
+    }
+}
+
+MONTH_VALUE = {
+    'fr':
+    {
+        'janvier': 1,
+        'février': 2,
+        'fevrier': 2,
+        'mars': 3,
+        'avril': 4,
+        'mai': 5,
+        'juin': 6,
+        'juillet': 7,
+        'août': 8,
+        'aout': 8,
+        'septembre': 9,
+        'octobre': 10,
+        'novembre': 11,
+        'décembre': 12,
+        'decembre': 12,
+    }
+}
+
+SHORT_MONTH_VALUE = {
+# Untested for now!
+    'fr':
+    {
+        'jan': 1,
+        'janv': 1,
+        'fév': 2,
+        'févr': 2,
+        'fev': 2,
+        'fevr': 2,
+        'mar': 3,
+        'avr': 4,
+        'juil': 7,
+        'juill': 7,
+        'sep': 9,
+        'sept': 9,
+        'oct': 10,
+        'nov': 11,
+        'dec': 12,
+        'déc': 12,
+    }
+}
--- datection/regexes.py
+++ datection/regexes.py
@@ -1,83 +1,13 @@
 # -*- coding: utf-8 -*-
 
 import re
-
+from datenames import *
 
 # words located before a date, giving hints about how it relates
 # to other dates
 DAYS_PREFIX = r'le'
+FR_WEEKDAY_NAMES = r'(%s)' % ('|'.join(WEEKDAY_VALUE['fr'].keys()))
 
-WEEKDAY_VALUE = {
-    'fr':
-    {
-        'lundi': 1,
-        'mardi': 2,
-        'mercredi': 3,
-        'jeudi': 4,
-        'vendredi': 5,
-        'samedi': 6,
-        'dimanche': 7,
-    }
-}
-
-SHORT_WEEKDAY_VALUE = {
-# Untested for now!
-    'fr':
-    {
-        'lun': 1,
-        'mar': 2,
-        'mer': 3,
-        'jeu': 4,
-        'ven': 5,
-        'sam': 6,
-        'dim': 7,
-    }
-}
-
-FR_weekday_nameS = r'(%s)' % ('|'.join(WEEKDAY_VALUE['fr'].keys()))
-MONTH_VALUE = {
-    'fr':
-    {
-        'janvier': 1,
-        'février': 2,
-        'fevrier': 2,
-        'mars': 3,
-        'avril': 4,
-        'mai': 5,
-        'juin': 6,
-        'juillet': 7,
-        'août': 8,
-        'aout': 8,
-        'septembre': 9,
-        'octobre': 10,
-        'novembre': 11,
-        'décembre': 12,
-        'decembre': 12,
-    }
-}
-
-SHORT_MONTH_VALUE = {
-# Untested for now!
-    'fr':
-    {
-        'jan': 1,
-        'janv': 1,
-        'fév': 2,
-        'févr': 2,
-        'fev': 2,
-        'fevr': 2,
-        'mar': 3,
-        'avr': 4,
-        'juil': 7,
-        'juill': 7,
-        'sep': 9,
-        'sept': 9,
-        'oct': 10,
-        'nov': 11,
-        'dec': 12,
-        'déc': 12,
-    }
-}
 
 # first the whole month number, then the abbreviated
 # otherwhise, 'oct' might be matched in 'october', thus breaking
