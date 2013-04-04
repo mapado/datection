c3d7aac828775664dcd5e53229be6978b0cb80f9
Balthazar Rouberol <balthazar.rouberol@mapado.com>
2013-04-04 11:54:31 +0200

    More documentation and docstrings
    Signed-off-by: Balthazar Rouberol <balthazar.rouberol@mapado.com>

--- datection/__init__.py
+++ datection/__init__.py
@@ -1,21 +1,25 @@
 # -*- coding: utf-8 -*-
 
 """
-Datection provides you with a parser that can extract and normalize
-date/time related expressions.
+Datection provides you with a parser that can extract, serialize and
+serialize date/time related expressions.
 
 For example, the french expression "le lundi 15 mars, de 15h30 à 16h"
 will be normalized into the following JSON structure:
-
 {
     'date': {'day': 15, 'month': 3, 'year': None},
     'time':
         {'end_time': {'hour': 16, 'minute': 0},
         'start_time': {'hour': 15,'minute': 30}
         }
-}
+},
+and the following python standard (and SQL-compliant) structure
+(
+    datetime.datetime(2013, 3, 15, 15, 30),
+    datetime.datetime(2013, 3, 15, 16, 0)
+)
 
-No external dependencies are needed.
+No external dependencies are required.
 """
 
 __version__ = '0.3.2'
--- datection/deserialize.py
+++ datection/deserialize.py
@@ -1,9 +1,18 @@
 # -*- coding: utf-8 -*-
 
+"""
+This module provides a deserialization functionality, able to recreate
+python objects from JSON-serialized timepoint structure.
+"""
+
 import serialize
 
 
 def deserialize(serialized):
+    """ Deserialize the input JSON strcture and return
+        a datection.serialize.Timepoint subclass instance.
+
+    """
     if serialized['timepoint'] == 'date':
         return create_date(serialized)
     elif serialized['timepoint'] == 'date_list':
@@ -21,6 +30,12 @@ def deserialize(serialized):
 
 
 def create_date(ser, timepoint=True):
+    """ Instanciate a datection.serialize.Date from a JSON structure.
+
+    If timepoint=True, then the returned DateTime instance will bear an
+    timepoint attribute with value 'date'.
+
+    """
     date = serialize.Date(
         day=ser['day'],
         month=ser['month'],
@@ -31,6 +46,7 @@ def create_date(ser, timepoint=True):
 
 
 def create_date_list(ser):
+    """ Instanciate a datection.serialize.DateList from a JSON structure. """
     dates = [create_date(d, timepoint=False) for d in ser['dates']]
     return serialize.DateList(
         dates=dates,
@@ -38,6 +54,12 @@ def create_date_list(ser):
 
 
 def create_date_interval(ser, timepoint=True):
+    """ Instanciate a datection.serialize.DateInterval from a JSON structure.
+
+    If timepoint=True, then the returned DateTime instance will bear an
+    timepoint attribute with value 'date_interval'.
+
+    """
     start_date = create_date(ser['start_date'], timepoint=False)
     end_date = create_date(ser['end_date'], timepoint=False)
     di = serialize.DateInterval(start_date=start_date, end_date=end_date)
@@ -47,6 +69,12 @@ def create_date_interval(ser, timepoint=True):
 
 
 def create_time(ser, timepoint=True):
+    """ Instanciate a datection.serialize.Time from a JSON structure.
+
+    If timepoint=True, then the returned DateTime instance will bear an
+    timepoint attribute with value 'time'.
+
+    """
     hour, minute = ser['hour'], ser['minute']
     time = serialize.Time(
         hour=hour,
@@ -57,6 +85,12 @@ def create_time(ser, timepoint=True):
 
 
 def create_time_interval(ser, timepoint=True):
+    """ Instanciate a datection.serialize.TimeInterval from a JSON structure.
+
+    If timepoint=True, then the returned DateTime instance will bear an
+    timepoint attribute with value 'time_interval'.
+
+    """
     start_time = create_time(ser['start_time'], timepoint=False)
     if ser['end_time'] is not None:
         end_time = create_time(ser['end_time'], timepoint=False)
@@ -69,6 +103,12 @@ def create_time_interval(ser, timepoint=True):
 
 
 def create_datetime(ser, timepoint=True):
+    """ Instanciate a datection.serialize.DateTime from a JSON structure.
+
+    If timepoint=True, then the returned DateTime instance will bear an
+    timepoint attribute with value 'datetime'.
+
+    """
     date = create_date(ser['date'], timepoint=False)
     time = create_time_interval(ser['time'], timepoint=False)
     dt = serialize.DateTime(date=date, time=time)
@@ -78,11 +118,19 @@ def create_datetime(ser, timepoint=True):
 
 
 def create_datetime_list(ser):
+    """ Instanciate a datection.serialize.DateTimeList from a
+        JSON structure.
+
+    """
     dts = [create_datetime(dt, timepoint=False) for dt in ser['datetimes']]
     return serialize.DateTimeList(datetimes=dts, timepoint=ser['timepoint'])
 
 
 def create_datetime_interval(ser):
+    """ Instanciate a datection.serialize.DateTimeInterval from a
+        JSON structure.
+
+    """
     di = create_date_interval(ser['date_interval'], timepoint=False)
     ti = create_time_interval(ser['time_interval'], timepoint=False)
     return serialize.DateTimeInterval(
