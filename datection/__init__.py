eef2b632bb71bb50c3f42d181ff8bb76953504a0
Balthazar Rouberol <balthazar.rouberol@mapado.com>
2013-07-08 10:05:31 +0200

    Package desciption update
    Signed-off-by: Balthazar Rouberol <balthazar.rouberol@mapado.com>

--- datection/__init__.py
+++ datection/__init__.py
@@ -1,29 +1,17 @@
 # -*- coding: utf-8 -*-
 
 """
-Datection provides you with a parser that can extract, serialize and
-serialize date/time related expressions.
+Datection provides you with a parser that can extract and normalize
+litteral date/time related expressions, from possibly several languages,
+and export them into python or database compliant formats.
 
-For example, the french expression "le lundi 15 mars, de 15h30 à 16h"
-will be normalized into the following JSON structure:
-{
-    'date': {'day': 15, 'month': 3, 'year': None},
-    'time':
-        {'end_time': {'hour': 16, 'minute': 0},
-        'start_time': {'hour': 15,'minute': 30}
-        }
-},
-and the following python standard (and SQL-compliant) structure
-(
-    datetime.datetime(2013, 3, 15, 15, 30),
-    datetime.datetime(2013, 3, 15, 16, 0)
-)
-
-No external dependencies are required.
+All expressions will be exported to rrules, as defined in the
+RFC 5545: https://tools.ietf.org/html/rfc5545 (prefered for database storage),
+and/or python rrules and datetimes objects.
 """
 
 __title__ = 'datection'
-__version__ = '0.6.2'
+__version__ = '0.6.3'
 __author__ = 'Balthazar Rouberol'
 
 from datection.parse import parse
