802f932700a8770788cbb2ff7d936d2356549a5c
Balthazar Rouberol <balthazar.rouberol@mapado.com>
2013-07-15 14:12:59 +0200

    Bump version to 0.6.5
    Signed-off-by: Balthazar Rouberol <balthazar.rouberol@mapado.com>

--- datection/__init__.py
+++ datection/__init__.py
@@ -11,7 +11,7 @@ and/or python rrules and datetimes objects.
 """
 
 __title__ = 'datection'
-__version__ = '0.6.4'
+__version__ = '0.6.5'
 __author__ = 'Balthazar Rouberol'
 
 from datection.parse import parse
