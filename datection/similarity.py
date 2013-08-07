ffcb136ac3a959f1143dcd7584746873c492e819
Balthazar Rouberol <balthazar.rouberol@mapado.com>
2013-08-07 11:16:57 +0200

    Calculation of a similarity score based on a combination of rrules
    Instead of working on an rrule basis, we have to be able to compare
    entire schedules. To do such a thing, we combine each attribute of
    all rrules:
     * weekdays
     * time span (discretised by 30 minutes intervals)
     * day span (discretised by 1 day intervals)
    
    The similarity score calculation follows the same principle:
    average of the 3 jaccard scores (one for each attribute)
    
    Signed-off-by: Balthazar Rouberol <balthazar.rouberol@mapado.com>

--- datection/similarity.py
+++ datection/similarity.py
@@ -15,10 +15,72 @@ def jaccard_distance(set1, set2):
     Return the length of the insersection of the 2 sets over the lengt
     of their union
     """
-    return len(set1.intersection(set2)) / len(set1.union(set2))
+    if not set1 and not set2:
+        return 1.
+    try:
+        return len(set1.intersection(set2)) / len(set1.union(set2))
+    except ZeroDivisionError:
+        return 0
 
-def weekday_similarity(rrule1, rrule2):
-    """Return the weekday similarity bewteen the two rrules
+
+def discretise_time_slot(rrule_struct):
+    """Discretise the time interval of rrule_struct by 30 minutes slots
+    """
+    out = []
+    rrule = rrulestr(rrule_struct['rrule'])
+    start_datetime = datetime.combine(
+        rrule.dtstart,
+        time(rrule.byhour[0], rrule.byminute[0]))
+    end_datetime = start_datetime + timedelta(
+        minutes=rrule_struct['duration'])
+    discrete_time = start_datetime
+    while discrete_time <= end_datetime:
+        out.append(discrete_time.time())
+        discrete_time += timedelta(minutes=30)
+    return out
+
+
+def discretise_day_interval(rrule):
+    """Discretise the day interval of rrule_struct by 1 day slots
+    """
+    out = []
+    discrete_day = rrule.dtstart
+    if not rrule.until:
+        return [rrule.dtstart]
+    while discrete_day <= rrule.until:
+        out.append(discrete_day.date())
+        discrete_day += timedelta(days=1)
+    return out
+
+
+def combine_weekdays(schedule):
+    weekdays = set()
+    for rrule_struct in schedule:
+        rrule = (rrulestr(rrule_struct['rrule']))
+        for wk in (w.weekday for w in rrule.byweekday):
+            weekdays.add(wk)
+    return weekdays
+
+
+def combine_duration(schedule):
+    day_span = set()
+    for rrule_struct in schedule:
+        rrule = (rrulestr(rrule_struct['rrule']))
+        for day in discretise_day_interval(rrule):
+            day_span.add(day)
+    return day_span
+
+
+def combine_schedule(schedule):
+    time_span = set()
+    for rrule_struct in schedule:
+        for t in discretise_time_slot(rrule_struct):
+            time_span.add(t)
+    return time_span
+
+
+def weekday_similarity(weekdays1, weekdays2):
+    """Return the weekday similarity bewteen the two weekday sets
 
     The weekday similarity is defined as the jaccard distance of the weekday
     set of each rrule.
@@ -27,13 +89,11 @@ def weekday_similarity(rrule1, rrule2):
     total similarity.
 
     """
-    weekdays1 = set([wk.weekday for wk in rrule1.byweekday])
-    weekdays2 = set([wk.weekday for wk in rrule2.byweekday])
     return jaccard_distance(weekdays1, weekdays2)
 
 
-def schedule_similarity(rrule_struct1, rrule_struct2):
-    """Return the schedule similarity bewteen the two rrules
+def schedule_similarity(time_slot1, time_slot2):
+    """Return the schedule similarity bewteen the two time slots set
 
     The weekday similarity is defined as the jaccard distance of the
     discretised time slots intervals set of each rrule_struct.
@@ -44,28 +104,10 @@ def schedule_similarity(rrule_struct1, rrule_struct2):
     total similarity.
 
     """
-    def discretise_time_slot(rrule_struct):
-        """Discretise the time interval of rrule_struct by 30 minutes slots
-        """
-        out = []
-        rrule = rrulestr(rrule_struct['rrule'])
-        start_datetime = datetime.combine(
-            rrule.dtstart,
-            time(rrule.byhour[0], rrule.byminute[0]))
-        end_datetime = start_datetime + timedelta(
-            minutes=rrule_struct['duration'])
-        discrete_time = start_datetime
-        while discrete_time <= end_datetime:
-            out.append(discrete_time)
-            discrete_time += timedelta(minutes=30)
-        return out
-
-    time_slot1 = set(discretise_time_slot(rrule_struct1))
-    time_slot2 = set(discretise_time_slot(rrule_struct2))
     return jaccard_distance(time_slot1, time_slot2)
 
 
-def duration_similarity(rrule1, rrule2):
+def duration_similarity(day_set1, day_set2):
     """Return the duration similarity bewteen the two rrules
 
     The schedule similarity is defined as the jaccard distance of the
@@ -75,24 +117,11 @@ def duration_similarity(rrule1, rrule2):
     total similarity.
 
     """
-    def discretise_day_interval(rrule):
-        """Discretise the day interval of rrule_struct by 1 day slots
-        """
-        out = []
-        discrete_day = rrule.dtstart
-        if not rrule.until:
-            return [rrule.dtstart]
-        while discrete_day <= rrule.until:
-            out.append(discrete_day)
-            discrete_day += timedelta(days=1)
-        return out
-    day_set1 = set(discretise_day_interval(rrule1))
-    day_set2 = set(discretise_day_interval(rrule2))
     return jaccard_distance(day_set1, day_set2)
 
 
-def rrule_similarity(rrule_struct1, rrule_struct2):
-    """Compute the similarity score bewteen the two argument rrule_struct
+def similarity(schedule1, schedule2):
+    """Compute the similarity score bewteen the two argument schedules
 
     The similarity score is expressed as a float between 0 and 1, 1 being
     total similarity, and is calculated as the average of the jaccard
@@ -102,38 +131,16 @@ def rrule_similarity(rrule_struct1, rrule_struct2):
      * duration (in days)
 
     """
-    rrule1 = rrulestr(rrule_struct1['rrule'])
-    rrule2 = rrulestr(rrule_struct2['rrule'])
-    wk_sim = weekday_similarity(rrule1, rrule2)
-    sc_sim = schedule_similarity(rrule_struct1, rrule_struct2)
-    du_sim = duration_similarity(rrule1, rrule2)
+    weekday_set1 = combine_weekdays(schedule1)
+    weekday_set2 = combine_weekdays(schedule2)
+    wk_sim = weekday_similarity(weekday_set1, weekday_set2)
+
+    day_set1 = combine_duration(schedule1)
+    day_set2 = combine_duration(schedule2)
+    du_sim = duration_similarity(day_set1, day_set2)
+
+    time_slots1 = combine_schedule(schedule1)
+    time_slots2 = combine_schedule(schedule2)
+    sc_sim = schedule_similarity(time_slots1, time_slots2)
+
     return sum((wk_sim, sc_sim, du_sim)) / 3
-
-
-def timepoint_similarity(rrule_struct1, rrule_struct2):
-    """Compute the similarity score bewteen the two argument rrule_struct
-
-    The similarity score is expressed as a float between 0 and 1, 1 being
-    total similarity, and is calculated as the average of the jaccard
-    similarity coefficient of the following attributes:
-     * schedule
-     * duration (in days)
-
-    """
-    rrule1 = rrulestr(rrule_struct1['rrule'])
-    rrule2 = rrulestr(rrule_struct2['rrule'])
-    sc_sim = schedule_similarity(rrule_struct1, rrule_struct2)
-    du_sim = duration_similarity(rrule1, rrule2)
-    return sum((sc_sim, du_sim)) / 2
-
-
-def similarity(rrule_struct1, rrule_struct2):
-    if ('BYDAY' in rrule_struct1['rrule']
-        and 'BYDAY' in rrule_struct2['rrule']):
-        return rrule_similarity(rrule_struct1, rrule_struct2)
-    elif ('BYDAY' not in rrule_struct1['rrule']
-        and 'BYDAY' not in rrule_struct2):
-        return timepoint_similarity(rrule_struct1, rrule_struct2)
-    else:
-        raise ValueError('RRule structures not comparable.'
-            'One is a recurrence, the other is simple timepoints.')
--- datection/test/test_similarity.py
+++ datection/test/test_similarity.py
@@ -10,9 +10,10 @@ import unittest
 
 from datection.similarity import *
 from dateutil.rrule import rrulestr
-
+from datetime import time, date
 
 class UtilsTest(unittest.TestCase):
+    """Test utility functions of the similarity module"""
     def test_jaccard_distance(self):
         s1 = set([0, 1, 2, 3])
         s2 = set([0, 1, 2, 4])
@@ -20,103 +21,118 @@ class UtilsTest(unittest.TestCase):
         self.assertEqual(jaccard_distance(s1, s2), 3/5)
         self.assertEqual(jaccard_distance(s1, s1), 1)
 
+    def test_discretise_day_interval(self):
+        rrule = rrulestr(('DTSTART:20130807\nRRULE:FREQ=WEEKLY;'
+            'BYHOUR=15;BYMINUTE=30;UNTIL=20130810'))
+        self.assertEqual(len(discretise_day_interval(rrule)), 4)
+        self.assertEqual(discretise_day_interval(rrule)[1], date(2013, 8, 8))
+
+    def test_discretise_time_slot(self):
+        struct  = {
+            'rrule': ('DTSTART:20130807\nRRULE:FREQ=WEEKLY;'
+            'BYHOUR=15;BYMINUTE=30;UNTIL=20130810'),
+            'duration': 180
+        }
+        self.assertEqual(len(discretise_time_slot(struct)), 7)
+        self.assertEqual(discretise_time_slot(struct)[1], time(16, 0))
+
+
+class AttributeCombiationTest(unittest.TestCase):
+    """Test the combination of several rrules"""
+    def setUp(self):
+        self.schedule = [{
+                'duration': 60,
+                'rrule': ('DTSTART:20130807\nRRULE:FREQ=WEEKLY;BYDAY=MO,TU,'
+                    'WE,TH,FR;BYHOUR=15;BYMINUTE=30;UNTIL=20130827')
+            },
+            {
+                'duration': 60,
+                'rrule': ('DTSTART:20130807\nRRULE:FREQ=WEEKLY;BYDAY=SA;'
+                    'BYHOUR=15;BYMINUTE=30;UNTIL=20130913')
+            }
+        ]
+
+    def test_combine_weekdays(self):
+        self.assertEqual(combine_weekdays(self.schedule), set(range(6)))
+
+    def test_combine_duration(self):
+        self.assertEqual(len(combine_duration(self.schedule)), 38)
+
+    def test_combine_duration_no_overlap(self):
+        no_overlap  = [
+            {
+                'duration': 0,
+                'rrule': ('DTSTART:20130807\nRRULE:FREQ=WEEKLY;BYDAY=MO,TU,'
+                    'WE,TH,FR;BYHOUR=15;BYMINUTE=30;UNTIL=20130810')
+            },
+            {
+                'duration': 0,
+                'rrule': ('DTSTART:20130907\nRRULE:FREQ=WEEKLY;BYDAY=MO,TU,'
+                    'WE,TH,FR;BYHOUR=15;BYMINUTE=30;UNTIL=20130910')
+            }
+        ]
+        self.assertEqual(
+            len(combine_duration(no_overlap)), 8)
+
+    def test_combine_time_interval(self):
+        self.assertEqual(len(combine_schedule(self.schedule)), 3)
+
+    def test_combine_time_interval_no_overlap(self):
+        no_overlap  = [
+            {
+                'duration': 0,
+                'rrule': ('DTSTART:20130807\nRRULE:FREQ=WEEKLY;BYDAY=MO,TU,'
+                    'WE,TH,FR;BYHOUR=15;BYMINUTE=30;UNTIL=20130810')
+            },
+            {
+                'duration': 0,
+                'rrule': ('DTSTART:20130907\nRRULE:FREQ=WEEKLY;BYDAY=MO,TU,'
+                    'WE,TH,FR;BYHOUR=15;BYMINUTE=30;UNTIL=20130910')
+            }
+        ]
+        self.assertEqual(
+            len(combine_schedule(no_overlap)), 1)
+
+
 
 class WeekdayRRuleSimilarityTest(unittest.TestCase):
     """Test similarity score calculation bewteen weekday recurrences"""
     def setUp(self):
-        self.rrule_struct1 = {
+        self.schedule1 = [{
             'duration': 180,
             'rrule': ('DTSTART:20130806\nRRULE:FREQ=WEEKLY;BYDAY=MO,TU,WE;'
                 'BYHOUR=5;BYMINUTE=0;UNTIL=20130826')
-            }
-        self.rrule1 = rrulestr(self.rrule_struct1['rrule'])
-        self.rrule_struct2 = {
+            }]
+        self.schedule2 = [{
             'duration': 600,
             'rrule': ('DTSTART:20130806\nRRULE:FREQ=WEEKLY;BYDAY=MO,TU,FR;'
                 'BYHOUR=4;BYMINUTE=0;UNTIL=20130816')
-            }
-        self.rrule2 = rrulestr(self.rrule_struct2['rrule'])
+            }]
 
     def test_weekday_similarity(self):
-        self.assertEqual(weekday_similarity(self.rrule1, self.rrule2), 2/4)
-        self.assertEqual(weekday_similarity(self.rrule1, self.rrule1), 1)
+        weekday_set1 = combine_weekdays(self.schedule1)
+        weekday_set2 = combine_weekdays(self.schedule2)
+        self.assertEqual(weekday_similarity(weekday_set1, weekday_set2), 2/4)
+        self.assertEqual(weekday_similarity(weekday_set1, weekday_set1), 1)
 
     def test_duration_similarity(self):
-        self.assertEqual(duration_similarity(self.rrule1, self.rrule2), 11/21)
-        self.assertEqual(duration_similarity(self.rrule1, self.rrule1), 1)
+        day_set1 = combine_duration(self.schedule1)
+        day_set2 = combine_duration(self.schedule2)
+        self.assertEqual(duration_similarity(day_set1, day_set2), 11/21)
+        self.assertEqual(duration_similarity(day_set1, day_set1), 1)
 
     def test_schedule_similarity(self):
+        time_slots_set1 = combine_schedule(self.schedule1)
+        time_slots_set2 = combine_schedule(self.schedule2)
         self.assertEqual(
-            schedule_similarity(self.rrule_struct1, self.rrule_struct2), 6/18)
+            schedule_similarity(time_slots_set1, time_slots_set2), 6/18)
         self.assertEqual(
-            schedule_similarity(self.rrule_struct1, self.rrule_struct1), 1)
+            schedule_similarity(time_slots_set1, time_slots_set1), 1)
 
     def test_rrule_similarity(self):
         avg = (2/4 + 11/21 + 6/18) / 3
         self.assertEqual(
-            round(rrule_similarity(self.rrule_struct1, self.rrule_struct2), 9),
+            round(similarity(self.schedule1, self.schedule2), 9),
             round(avg, 9))
         self.assertEqual(
-            similarity(self.rrule_struct1, self.rrule_struct1), 1)
-
-    def test_similarity_with_recurrences(self):
-        avg = (2/4 + 11/21 + 6/18) / 3
-        self.assertEqual(
-            round(similarity(self.rrule_struct1, self.rrule_struct2), 9),
-            round(avg, 9))
-        self.assertEqual(
-            similarity(self.rrule_struct1, self.rrule_struct1), 1)
-
-
-class TimepointRRuleSimilarityTest(unittest.TestCase):
-    """Test similarity score calculation bewteen timepoints"""
-    def setUp(self):
-        self.rrule_struct1 = {
-            'duration': 0,
-            'rrule': ('DTSTART:20140305\nRRULE:FREQ=DAILY;COUNT=1;'
-                'BYMINUTE=30;BYHOUR=20')
-        }
-        self.rrule1 = rrulestr(self.rrule_struct1['rrule'])
-        self.rrule_struct2 = {
-            'duration': 0,
-            'rrule': ('DTSTART:20140305\nRRULE:FREQ=DAILY;COUNT=1;'
-                'BYMINUTE=30;BYHOUR=21')
-        }
-        self.rrule2 = rrulestr(self.rrule_struct2['rrule'])
-
-    def test_duration_similarity(self):
-        self.assertEqual(duration_similarity(self.rrule1, self.rrule2), 1)
-        self.assertEqual(duration_similarity(self.rrule1, self.rrule1), 1)
-
-    def test_schedule_similarity(self):
-        self.assertEqual(
-            schedule_similarity(self.rrule_struct1, self.rrule_struct2), 0)
-        self.assertEqual(
-            schedule_similarity(self.rrule_struct1, self.rrule_struct1), 1)
-
-    def test_similarity_with_recurrences(self):
-        avg = (1 + 0) / 2
-        self.assertEqual(
-            round(similarity(self.rrule_struct1, self.rrule_struct2), 9),
-            round(avg, 9))
-        self.assertEqual(
-            similarity(self.rrule_struct1, self.rrule_struct1), 1)
-
-
-class VariousRRuleSimilarityTest(unittest.TestCase):
-
-    def setUp(self):
-        self.rrule_struct1 = {
-                'duration': 0,
-                'rrule': ('DTSTART:20140305\nRRULE:FREQ=DAILY;COUNT=1;'
-                    'BYMINUTE=30;BYHOUR=20')
-            }
-        self.rrule_struct2 = {
-                'duration': 600,
-                'rrule': ('DTSTART:20130806\nRRULE:FREQ=WEEKLY;BYDAY=MO,TU,FR;'
-                    'BYHOUR=4;BYMINUTE=0;UNTIL=20130816')
-            }
-
-    def tets_similarity(self):
-        with self.assertRaises(ValueError):
-            similarity(self.rrule_struct1, self.rrule_struct2)
+            similarity(self.schedule1, self.schedule1), 1)
