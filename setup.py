1e6fe49333dc248f374d8b53e0a3054d9c2d6314
Balthazar Rouberol <balthazar.rouberol@mapado.com>
2013-08-22 11:26:48 +0200

    Include translation files at installation Signed-off-by: Balthazar Rouberol <balthazar.rouberol@mapado.com>

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ import datection
 from setuptools import setup, find_packages
 
 setup(
-        name="datection",
-            version=datection.__version__,
-            description='Parse strings and extract normalized temporal data.',
-            author=["Balthazar Rouberol"],
-            author_email=['balthazar@mapado.com'],
-            packages=find_packages(),
-            test_suite='nose.collector',
-            tests_require=['nose'],
-            )
+    name="datection",
+    version=datection.__version__,
+    description='Parse strings and extract normalized temporal data.',
+    author=["Balthazar Rouberol"],
+    author_email=['balthazar@mapado.com'],
+    packages=find_packages(),
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    )
