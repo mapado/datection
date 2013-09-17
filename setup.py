3bbd847e03448b941c54540a64fc95028684ee5e
Balthazar Rouberol <balthazar.rouberol@mapado.com>
2013-09-17 16:05:30 +0200

    Fix requirements.txt and setup.py
    Signed-off-by: Balthazar Rouberol <balthazar.rouberol@mapado.com>

--- setup.py
+++ setup.py
@@ -1,21 +1,14 @@
-import re
-
-from os.path import dirname, join
 from setuptools import setup, find_packages
-
-
-version = re.search("__version__ = '([^']+)'", open(
-    join(dirname(__file__), 'datection', '__init__.py')
-).read().strip()).group(1)
 
 setup(
     name="datection",
-    version=version,
+    version='0.8.0',
     description='Parse strings and extract normalized temporal data.',
     author=["Balthazar Rouberol"],
     author_email=['balthazar@mapado.com'],
     packages=find_packages(),
     # test_suite='nose.collector'
     # tests_require=['nose'],
-    include_package_data=True
+    include_package_data=True,
+    install_requires=['python-dateutil'],
 )
