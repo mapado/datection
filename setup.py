c77ee2226711e366ebf5a411e0868e3896262ac8
Balthazar Rouberol <balthazar.rouberol@mapado.com>
2013-09-19 11:52:51 +0200

    Update python-dateutil req to dateutil to avoid collision with public package on PyPI
    Signed-off-by: Balthazar Rouberol <balthazar.rouberol@mapado.com>

--- setup.py
+++ setup.py
@@ -10,5 +10,5 @@ setup(
     # test_suite='nose.collector'
     # tests_require=['nose'],
     include_package_data=True,
-    install_requires=['python-dateutil'],
+    install_requires=['dateutil'],
 )
