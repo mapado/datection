bbd58c8a6f206900259a4eb719e1c1b446a18288
Balthazar Rouberol <balthazar.rouberol@mapado.com>
2014-01-07 16:13:05 +0100

    Bump version: 1.4.1 → 1.4.2

--- datection/__init__.py
+++ datection/__init__.py
@@ -11,7 +11,7 @@ and/or python rrules and datetimes objects.
 """
 
 __title__ = 'datection'
-__version__ = '1.4.1'
+__version__ = '1.4.2'
 __author__ = 'Balthazar Rouberol'
 
 from datection.parse import parse
