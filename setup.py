e244732d03f04b79fecb9db9d0c9f9091c2e47d4
Julien Deniau <julien.deniau@gmail.com>
2014-08-27 10:18:54 +0200

    Bump version: 1.14.4 → 1.14.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="datection",
-    version='1.14.4',
+    version='1.14.5',
     description='Parse strings and extract normalized temporal data.',
     author=["Balthazar Rouberol"],
     author_email=['balthazar@mapado.com'],
