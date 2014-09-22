14d673e4b500777d417642ad3e5693f6410d1d0d
Balthazar Rouberol <balthazar.rouberol@mapado.com>
2014-09-22 14:00:43 +0200

    Bump version: 1.14.5 → 2.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="datection",
-    version='1.14.5',
+    version='2.0.0',
     description='Parse strings and extract normalized temporal data.',
     author=["Balthazar Rouberol"],
     author_email=['balthazar@mapado.com'],
