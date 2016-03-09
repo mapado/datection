044126d83658255985c9dd29baee1a9871608362
epi <julien.balian@mapado.com>
2016-03-09 14:37:52 +0100

    Better handle locale

--- datection/lang.py
+++ datection/lang.py
@@ -5,6 +5,7 @@ Definition of default locales for given languages
 """
 
 import re
+from datection.tokenize import Tokenizer
 
 
 DEFAULT_LOCALES = {
@@ -22,5 +23,14 @@ def detect_language(text, lang):
     """ Language check to detect locale """
     if re.search(r' (am|pm) ', text):
         return 'en'
+
+    if lang not in DEFAULT_LOCALES:
+        tok_len_lang, proposed_lang = max(
+            (len(Tokenizer(text, key).tokenize()), key)
+            for key in DEFAULT_LOCALES.keys()
+        )
+
+        if tok_len_lang:
+            lang = proposed_lang
 
     return lang
