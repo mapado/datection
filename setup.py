4d448a200fef51499ba3fb43a74dabaef4304d4a
Julien Balian <julien.balian@mapado.com>
2016-04-11 16:23:23 +0200

    Bump version: 2.8.2 → 2.8.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="datection",
-    version='2.8.2',
+    version='2.8.3',
     description='Parse strings and extract normalized temporal data.',
     author=["Balthazar Rouberol"],
     author_email=['balthazar@mapado.com'],
