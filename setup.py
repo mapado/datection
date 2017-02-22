f68a4feebe667679836e3bb80cfb0a2261ffe1ef
Romain SENESI <romain.senesi@mapado.com>
2017-02-22 16:04:03 +0100

    Bump version: 3.1.0 → 3.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="datection",
-    version='3.1.0',
+    version='3.1.1',
     description='Parse strings and extract normalized temporal data.',
     author=["Balthazar Rouberol"],
     author_email=['balthazar@mapado.com'],
