bfeb249675d77cc25138cd47b7f04172e8fdfd37
Romain SENESI <romain.senesi@mapado.com>
2018-04-10 14:57:59 +0200

    Bump version: 3.11.4 → 3.11.5

--- datection/__init__.py
+++ datection/__init__.py
@@ -11,7 +11,7 @@ and/or python rrules and datetimes objects.
 """
 
 __title__ = 'datection'
-__version__ = '3.11.4'
+__version__ = '3.11.5'
 __author__ = 'Balthazar Rouberol'
 
 from datection.parse import parse
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="datection",
-    version='3.11.4',
+    version='3.11.5',
     description='Parse strings and extract normalized temporal data.',
     author=["Balthazar Rouberol"],
     author_email=['balthazar@mapado.com'],
