0d68f75b6382a677939b735331d5f30c63347db3
Romain SENESI <romain.senesi@mapado.com>
2019-05-23 15:41:51 +0200

    Bump version: 3.17.1 → 4.0.0

--- datection/__init__.py
+++ datection/__init__.py
@@ -11,7 +11,7 @@ and/or python rrules and datetimes objects.
 """
 
 __title__ = 'datection'
-__version__ = '3.17.1'
+__version__ = '4.0.0'
 __author__ = 'Balthazar Rouberol'
 
 from datection.parse import parse
